d8f733100cbecdf70db75270935e75f64d3a2f00
yeyoung8805 <yyk8805@naver.com>
2022-04-15 17:57:41 +0900

    make movie dummy data based on Movie model

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_clone/model/model_movie.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title' : '사랑의 불시착',
+      'keyword' : '사랑/로맨스/판타지',
+      'poster' : 'test_movie_1.png',
+      'like' : false
+    })
+  ];
   @override
   void initState() {
     super.initState();
