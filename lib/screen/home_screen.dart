5a025a8002ba1dadca00f918cbcd35502e7b4947
yeyoung8805 <yyk8805@naver.com>
2022-04-17 18:45:57 +0900

    add circle/box slider in home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_clone/model/model_movie.dart';
+import 'package:flutter_netflix_clone/widget/box_slider.dart';
 import 'package:flutter_netflix_clone/widget/carousel_slider.dart';
+import 'package:flutter_netflix_clone/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -48,7 +50,13 @@ class _HomeScreenState extends State<HomeScreen> {
             CarouselImage(movies: movies),
             TopBar(),
           ],
-        )
+        ),
+        CircleSlider(
+          movies: movies,
+        ),
+        BoxSlider(
+          movies: movies,
+        ),
       ],
     );
   }
