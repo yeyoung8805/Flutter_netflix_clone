ace1f32924c6a7b506a646277b0177850d7055a1
yeyoung8805 <yyk8805@naver.com>
2022-04-18 15:44:22 +0900

    add making detail screen in info, box/circle slider

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_clone/model/model_movie.dart';
+import 'package:flutter_netflix_clone/screen/detail_screen.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie>? movies;
@@ -104,7 +105,16 @@ class _CarouselImageState extends State<CarouselImage> {
                     children: <Widget>[
                       IconButton(
                         icon: Icon(Icons.info),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                            fullscreenDialog: true,
+                            builder: (BuildContext context) {
+                              return DetailScreen(
+                                movie: movies![_currentPage],
+                              );
+                            }
+                          ));
+                        },
                       ),
                       Text(
                         '정보',
