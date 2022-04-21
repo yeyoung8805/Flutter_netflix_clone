364b37d56a808c2d420b672b1f4477217f9635b9
yeyoung8805 <yyk8805@naver.com>
2022-04-21 15:35:50 +0900

    add Search (page and functions) not working...

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_clone/screen/home_screen.dart';
 import 'package:flutter_netflix_clone/screen/more_screen.dart';
+import 'package:flutter_netflix_clone/screen/search_screen.dart';
 import 'package:flutter_netflix_clone/widget/bottom_bar.dart';
 
 void main() {
@@ -30,11 +31,7 @@ class _MyAppState extends State<MyApp> {
             physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
               HomeScreen(),
-              Container(
-                child: Center(
-                  child: Text('search'),
-                ),
-              ),
+              SearchScreen(),
               Container(
                 child: Center(
                   child: Text('save'),
